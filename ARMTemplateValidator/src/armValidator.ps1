b3fcc4b96471e4209a38c3ba52592b39f98add64
lturmel <lturmel@expertasolutions.com>
2019-02-05 21:37:00 -0500

    subscription parameter addition
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

---
+++ ARMTemplateValidator/src/armValidator.ps1
@@ -0,0 +1,11 @@
+param(
+  [Parameter(Mandatory=$true, Position=1)]
+  [string]$subscriptionId
+, [Parameter(Mandatory=$true, Position=2)]
+  [string]$templateFile
+, [Parameter(Mandatory=$true, Position=3)]
+  [string]$parameterFile
+)
+
+write-host $templateFile
+write-host $parameterFile
