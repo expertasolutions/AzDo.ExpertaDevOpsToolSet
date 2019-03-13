4a609bf72b639d85cabe5d726c1f5a1c21226d84
lturmel <lturmel@expertasolutions.com>
2019-03-13 19:57:56 -0400

    Update appInsight.ps1

--- AzureAppInsight/src/appInsight.ps1
+++ AzureAppInsight/src/appInsight.ps1
@@ -8,14 +8,14 @@ param(
 , [Parameter(Mandatory=$true, Position=4)]
   [string]$tenantId
 , [Parameter(Mandatory=$true, Position=5)]
-  [string]$appInsigthName
+  [string]$appInsightName
 )
 
 $loginResult = az login --service-principal -u $servicePrincipalId -p $servicePrincipalKey --tenant $tenantId
 $setSubResult = az account set --subscription $subscriptionId
 
 $result = az resource list --namespace microsoft.insights --resource-type components --subscription $subscriptionId | ConvertFrom-Json
-$appInsight = $result | Where-Object { $_.name -eq "$(AppName)$(envName)" }
+$appInsight = $result | Where-Object { $_.name -eq "$appInsightName" }
 
 $id = az resource show --id $appInsight.id --query properties.InstrumentationKey --o tsv --Subscription $subscriptionId
 write-host "Instrmentation Key: $id"
