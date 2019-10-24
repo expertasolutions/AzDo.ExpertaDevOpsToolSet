9849754ae4f817e6b1a1694ce78a1f8c771d67be
lturmel <lturmel@expertasolutions.com>
2019-10-23 20:54:35 -0400

    Update uploadSecret.js

--- ManageKeyVaultSecrets/src/uploadSecret.js
+++ ManageKeyVaultSecrets/src/uploadSecret.js
@@ -54,7 +54,7 @@ try {
                     client = new KeyVault.KeyVaultClient(creds);
                     for(var s=0;s<secretsContent.length;s++){
                         client.setSecret(url, secretsContent[s].secret, secretsContent[s].value, s=> {
-                            console.log(secretsContent[s].secret + " created/updated");
+                            console.log(s.secret + " set with new value");
                         });
                     }
                 });
