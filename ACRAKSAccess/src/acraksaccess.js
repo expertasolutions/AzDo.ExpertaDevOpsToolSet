59dfe3c783ee84d89d4325a082abbe2de1ed0994
Louis Turmel <46848645+lturmel@users.noreply.github.com>
2019-11-05 22:35:34 -0500

    Issue0028 (#32)
    * test replacement
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * nodejs update
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * update
    
    * update
    
    * update
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * change
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * update
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * Update uploadSecret.js
    
    * azure-arm-resource usage
    
    * Update task.json
    
    * put task to preview (remove powershellcore)
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * Update appInsight.js
    
    * Update appInsight.js
    
    * update task
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * update
    
    * Update appInsight.js
    
    * Update appInsight.js
    
    * Update appInsight.js
    
    * azure-arm-containerregistry implementation
    
    * Update package.json
    
    * azure-pipelines-task-lib update
    
    * Update acrcred.js
    
    * Update acrcred.js
    
    * fix task
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * Update acrcred.js
    
    * update
    
    * implementation start
    
    * update
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * add @azure/ms-rest-nodeauth
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * add @azure/arm-resources
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * add @azure/arm-authorization
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * update fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix try
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * aks fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * task fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix task
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * get acr instance
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix aksclientid
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix try
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * output console
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * test
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * out
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * add role implementation
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * typo fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fixing script
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * fix
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * test
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * test
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * remove debug output
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>
    
    * parms output restructured
    
    Signed-off-by: lturmel <lturmel@expertasolutions.com>

--- ACRAKSAccess/src/acraksaccess.js
+++ ACRAKSAccess/src/acraksaccess.js
@@ -10,7 +10,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 
 var tl = require('azure-pipelines-task-lib');
-var shell = require('node-powershell');
+const msRestNodeAuth = require('@azure/ms-rest-nodeauth');
+const resourceManagement = require('@azure/arm-resources');
+const auth = require('@azure/arm-authorization');
+const graph = require('@azure/graph');
 
 try {
     var acrSubscriptionEndpoint = tl.getInput("acrSubscriptionEndpoint", true);
@@ -40,6 +43,7 @@ try {
     console.log("ACR Tenant Id: " + acrTenantId);
     /* End of ACR Subscription Informations */
 
+    console.log("");
     /* AKS Subscription Informations */
     console.log("AKS Azure Subscription Id: " + aksSubcriptionId);
     console.log("AKS ServicePrincipalId: " + aksServicePrincipalId);
@@ -47,39 +51,146 @@ try {
     console.log("AKS Tenant Id: " + aksTenantId);
     /* End of AKS Subscription Informations */
 
+    console.log("");
     console.log("Register mode: " + registerMode);
     console.log("ACR Resource Group: '" + acrResourceGroup + "'");
     console.log("Container Registry: " + containerRegistry);
     console.log("AKS Resource Group: " + aksResourceGroup);
     console.log("AKS Cluster: " + aksCluster);
-    console.log("ACR Username: " + acrUsername);
-    console.log("ACR Password: " + acrPassword);
-
-    // TODO: Use npm module to interact with azure container registry
-    var pwsh = new shell({ executionPolicy: 'Bypass', noProfile: true });
-
-    pwsh.addCommand(__dirname  + "/acraksaccess.ps1 -acrSubscriptionId '" + acrSubcriptionId + "' "
-        + "-acrServicePrincipalId '" + acrServicePrincipalId + "' -acrServicePrincipalKey '" + acrServicePrincipalKey + "' "
-        + "-acrTenantId '" + acrTenantId + "' "
-        + "-registerMode '" + registerMode + "' "
-        + "-acrResourceGroup '" + acrResourceGroup + "' "
-        + "-containerRegistry '" + containerRegistry + "' "
-        + "-aksResourceGroup '" + aksResourceGroup + "' "
-        + "-aksCluster '" + aksCluster + "' "
-        + "-acrUsername '" + acrUsername + "' "
-        + "-acrPwd '" + acrPassword + "' "
-        + "-aksSubscriptionId '" + aksSubcriptionId + "' "
-        ).then(function() {
-        return pwsh.invoke();
-    }).then(function(output){
-        console.log(output);
-        pwsh.dispose();
-    }).catch(function(err){
-        console.log(err);
-        tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
-        pwsh.dispose();
-    });
     
+    if(acrUsername != undefined){
+        console.log("ACR Username: " + acrUsername);
+        console.log("ACR Password: " + acrPassword);
+    }
+    // TODO: Implement codes here :P
+    msRestNodeAuth.loginWithServicePrincipalSecret (
+        aksServicePrincipalId, aksServicePrincipalKey, aksTenantId
+    ).then(aksCreds => {
+        if(registerMode == "aksSecret") {
+            console.log("AKS Secret Access mode");
+            throw new Error("AKS Secret access mode not implemented yet");
+            /*
+                Old Powershell code algo
+                $acrInfo = az acr show --name $containerRegistry -g $acrResourceGroup --subscription $acrSubscriptionId | ConvertFrom-Json
+                if(-not $acrInfo.adminUserEnabled){
+                    throw "Container registry named '$containerRegistry' does not have adminUser configured"
+                }
+            */
+        } else {
+            console.log("RBAC Access mode");
+            console.log("Looking for Azure Kubernetes service cluster ...");
+            
+            const aksResourceClient = new resourceManagement.ResourceManagementClient(aksCreds, aksSubcriptionId);
+            aksResourceClient.resources.list()
+                .then(result => {
+                    // Find the AKS Cluster Resource group
+                    const aksClusterInstance = result.find(element => {
+                        return element.name == aksCluster;
+                    });
+
+                    aksResourceClient.resources.getById(aksClusterInstance.id, '2019-10-01')
+                    .then(aksInfoResult => {
+                        const clientId = aksInfoResult.properties.servicePrincipalProfile.clientId;
+                        var aksAppCreds = new msRestNodeAuth.ApplicationTokenCredentials(aksCreds.clientId, aksTenantId, aksCreds.secret, 'graph');
+                        const aksGraphClient = new graph.GraphRbacManagementClient(aksAppCreds, aksTenantId, { baseUri: 'https://graph.windows.net' });
+                        var aksFilterValue = "appId eq '" + clientId + "'";
+                        var aksServiceFilter = {
+                            filter: aksFilterValue
+                        };
+                        // Get the AKS Service Principal details
+                        aksGraphClient.servicePrincipals.list(aksServiceFilter)
+                            .then(aksSearch => {
+                                const aksServicePrincipal = aksSearch.find(element => {
+                                    return element.appId == clientId;
+                                });
+
+                                if(aksServicePrincipal == undefined)
+                                {
+                                    throw new Error("AKS Server Principal not found");
+                                }
+
+                                // Get the Azure Container Registry resource infos
+                                msRestNodeAuth.loginWithServicePrincipalSecret(acrServicePrincipalId, acrServicePrincipalKey, acrTenantId
+                                ).then(acrCreds => {
+                                    
+                                    const acrResourceClient = new resourceManagement.ResourceManagementClient(acrCreds, acrSubcriptionId);
+                                    acrResourceClient.resources.list()
+                                    .then(acrResult => {
+                                        const acrInstance = acrResult.find(element => {
+                                            return element.name == containerRegistry;
+                                        });
+
+                                        if(acrInstance == undefined){
+                                            throw new Error("ACR Instance not found");
+                                        }
+
+                                        const acrAuthClient = new auth.AuthorizationManagementClient(acrCreds, acrSubcriptionId);
+                                        const acrPullRoleName = "AcrPull";
+
+                                        acrAuthClient.roleDefinitions.list("/")
+                                        .then(roles => {
+                                            var acrRole = roles.find(role => {
+                                                return role.roleName == acrPullRoleName;
+                                            });
+
+                                            if(acrRole == undefined){
+                                                throw new Error("AcrPull not found");
+                                            }
+
+                                            acrAuthClient.roleAssignments.listForResourceGroup(acrResourceGroup)
+                                            .then(rs => {
+                                                
+                                                var roleAssignement = rs.find(elm => {
+                                                    const rolId = "/subscriptions/" + acrSubcriptionId + acrRole.id;
+                                                    return rolId === elm.roleDefinitionId && elm.principalId === aksServicePrincipal.objectId;
+                                                });
+
+                                                if(roleAssignement == undefined){
+                                                    var newRoleParm = {
+                                                        roleDefinitionId: acrRole.id,
+                                                        principalId: aksServicePrincipal.objectId
+                                                    };
+
+                                                    console.log(newRoleParm);
+
+                                                    acrAuthClient.roleAssignments.create(acrInstance.id, aksServicePrincipal.objectId, newRoleParm)
+                                                        .then(newRoleResult => {
+                                                            console.log("New role assignement created !");
+                                                        }).catch(err=> {
+                                                            tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                                                        });
+                                                } else {
+                                                    console.log("Role assignement already existing");
+                                                }
+                                            }).catch(err => {
+                                                tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                                            });
+                                        }).catch(err => {
+                                            tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                                        });
+                                    })
+                                    .catch(err => {
+                                        tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                                    })
+                                }).catch(err => {
+                                    tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                                });
+                            })
+                            .catch(err => {
+                                tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                            });
+                    })
+                    .catch(err => {
+                        tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                    });
+                })
+                .catch(err => {
+                    tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+                });
+        }
+    }).catch(err => {
+        tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
+    });
 } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed');
 }
