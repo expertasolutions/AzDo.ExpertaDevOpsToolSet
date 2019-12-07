2a3c9a60d08db7791a34ac45550ba32ea0cb5bb3
lturmel <lturmel@expertasolutions.com>
2019-12-06 20:00:27 -0500

    raise error

--- ManageKeyVaultSecrets/src/uploadSecret.js
+++ ManageKeyVaultSecrets/src/uploadSecret.js
@@ -18,7 +18,7 @@ const ApplicationTokenCredentials = require('@azure/ms-rest-nodeauth').Applicati
 
 try {
 
-    tl.error("This task has been move to: https://marketplace.visualstudio.com/items?itemName=experta.AzureKeyVaultExtenion");
+    tl.setResult(tl.TaskResult.Failed, "This task has been move to: https://marketplace.visualstudio.com/items?itemName=experta.AzureKeyVaultExtenion");
 
     var azureSubscriptionEndpoint = tl.getInput("azureSubscriptionEndpoint", true);
     
